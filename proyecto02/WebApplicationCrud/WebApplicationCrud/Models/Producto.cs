001f926d780cb5e0bf9715be87e990b02f51e581
Franklin Cappa Ticona <franklincappa@gmail.com>
2026-07-11 19:22:50 -0500

    proyecto02

---
+++ proyecto02/WebApplicationCrud/WebApplicationCrud/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationCrud.Models
+{
+    public class Producto
+    {
+        [Display(Name = "Id Producto")] public int idproducto { get; set; }
+        [Display(Name = "Descripciòn")] public string descripcion { get; set; }
+        [Display(Name = "Medida")] public string umedida { get; set; }
+        [Display(Name = "Precio")] public decimal precio { get; set; }
+        [Display(Name = "Stock")] public int stock { get; set; }
+
+    }
+}
