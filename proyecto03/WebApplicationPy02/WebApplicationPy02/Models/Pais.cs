d1c819a5b82156320c3002ca94227a19a2b31ae0
Franklin Cappa Ticona <franklincappa@gmail.com>
2026-07-22 19:44:22 -0500

    update proyecto

---
+++ proyecto03/WebApplicationPy02/WebApplicationPy02/Models/Pais.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplicationPy02.Models
+{
+    public class Pais
+    {
+        public int idPais { get; set; }
+        public string ? nombre { get; set; }
+    }
+}
---
+++ proyecto03/WebApplicationPy02/WebApplicationPy02/Models/Vendedor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationPy02.Models
+{
+    public class Vendedor
+    {
+        [Display(Name = "Id Vendedor")] public int idVendedor { get; set; }
+        [Display(Name = "Nombres")] public string? nombre { get; set; }
+        [Display(Name = "Direción")] public string? direccion { get; set; }
+        [Display(Name = "Id Pais")] public string? idPais { get; set; }
+        [Required, DataType(DataType.EmailAddress),Display(Name = "Email")] public string? email { get; set; }
+
+    }
+}
