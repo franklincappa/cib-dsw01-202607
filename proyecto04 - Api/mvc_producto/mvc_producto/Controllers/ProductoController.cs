bd741e9c104f5efbe1a85e8812b6f88b66e496af
Franklin Cappa Ticona <franklincappa@gmail.com>
2026-08-05 21:05:20 -0500

    Proyecto 02 mvc - consumo API

---
+++ proyecto04 - Api/mvc_producto/mvc_producto/Controllers/ProductoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc_producto.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+
+namespace mvc_producto.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Producto> listadoProductos = new List<Producto>();
+            using (var cli = new HttpClient())
+            {
+                cli.BaseAddress = new Uri("https://localhost:7060/");
+                HttpResponseMessage response = await cli.GetAsync("api/productos");
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                listadoProductos = JsonConvert.DeserializeObject<List<Producto>>(apiResponse).ToList();
+            }
+            return View(await Task.Run(() => listadoProductos));
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View(await Task.Run(() => new Producto()));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(producto);
+            }
+
+            string mensaje = "";
+
+            //Impactar sobre API
+            using (var cli = new HttpClient())
+            {
+                cli.BaseAddress = new Uri("https://localhost:7060/");
+                StringContent content = new StringContent( JsonConvert.SerializeObject(producto), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await cli.PostAsync("api/productos", content);
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                mensaje = apiResponse;
+            }
+
+            TempData["mensaje"] = mensaje;
+            return View(await Task.Run(()=> producto));
+
+        }
+
+
+
+
+    }
+}
